50eaaf559f872592a07153ed18411f23111ea1f2
ebbyandflow <89115074+ebbyandflow@users.noreply.github.com>
2021-09-06 16:35:46 -0700

    lots of change

---
+++ script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+    });
+    //toggle menu/navbar script
+     $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active")
+        $('.menu-btn i.active').toggleClass("active")
+    });
+});
+
+//typing animation script
+var typed = new Typed(".typing", {
+ strings: ["Learning Experience Designer", "Project Manager", "Data Visualizer ", "Consultant"],
+ typeSpeed: 50,
+ backSpeed: 30, 
+ loop: true
+});
+var typed = new Typed(".typing-2", {
+    strings: ["Learning Experience Designer", "Freelancer Consultant", "Instructional Designer"],
+    typeSpeed: 30,
+    backSpeed: 50, 
+    loop: true
+   });
